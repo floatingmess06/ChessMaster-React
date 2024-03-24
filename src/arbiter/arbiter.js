bbc48f81781fafcb7a85db9c23f931d6b8dbf2f1
floatingmess06 <amandashore2468@gmail.com>
2024-03-24 10:07:03 +0530

    C11 // Methods Improvement for proper modularisation

--- src/arbiter/arbiter.js
+++ src/arbiter/arbiter.js
@@ -1,31 +1,45 @@
-import { getRookMoves, getKnightMoves, getBishopMoves, getQueenMoves, getKingMoves, getPawnMoves, getPawnCaptures} from "./getMoves";
+import {
+  getRookMoves,
+  getKnightMoves,
+  getBishopMoves,
+  getQueenMoves,
+  getKingMoves,
+  getPawnMoves,
+  getPawnCaptures,
+} from "./getMoves";
+import {movePiece, movePawn} from './move'
 
 const arbiter = {
-    getRegularMoves: function({position, piece, rank,file}){
-
-        if(piece.endsWith('n'))
-            return  getKnightMoves({position, rank,file})
-        else if(piece.endsWith('r'))
-            return getRookMoves({position, piece, rank,file})
-        else if(piece.endsWith('b'))
-            return getBishopMoves({position, piece, rank,file})
-        else if(piece.endsWith('q'))
-            return getQueenMoves({position, piece, rank,file})
-        else if(piece.endsWith('k'))
-            return getKingMoves({position, piece, rank,file})
-        else if(piece.endsWith('p'))
-            return getPawnMoves({position, piece, rank,file})
-    },
-    getValidMoves: function({position,prevPosition, piece, rank,file}){
-        let moves = this.getRegularMoves({position, piece, rank,file})
-        if(piece.endsWith('p')){
-            moves = [
-                ...moves,
-                ...getPawnCaptures({position,prevPosition, piece, rank,file})
-            ]
-        }
-        return moves
+  getRegularMoves: function ({ position, piece, rank, file }) {
+    if (piece.endsWith("n")) return getKnightMoves({ position, rank, file });
+    else if (piece.endsWith("r"))
+      return getRookMoves({ position, piece, rank, file });
+    else if (piece.endsWith("b"))
+      return getBishopMoves({ position, piece, rank, file });
+    else if (piece.endsWith("q"))
+      return getQueenMoves({ position, piece, rank, file });
+    else if (piece.endsWith("k"))
+      return getKingMoves({ position, piece, rank, file });
+    else if (piece.endsWith("p"))
+      return getPawnMoves({ position, piece, rank, file });
+  },
+  getValidMoves: function ({ position, prevPosition, piece, rank, file }) {
+    let moves = this.getRegularMoves({ position, piece, rank, file });
+    if (piece.endsWith("p")) {
+      moves = [
+        ...moves,
+        ...getPawnCaptures({ position, prevPosition, piece, rank, file }),
+      ];
     }
-}
+    return moves;
+  },
+  performMove: function ({position,piece,rank,file,x,y}) {
+        if(piece.endsWith('p') ){
+            return movePawn({position,piece,rank,file,x,y})
+        }else{
+            return movePiece({position,piece,rank,file,x,y})
+        }
+  },
+};
 
-export default arbiter
+export default arbiter;
--- src/components/Pieces/Pieces.js
+++ src/components/Pieces/Pieces.js
@@ -1,63 +1,75 @@
-import './Pieces.css'
-import Piece from './Piece'
-import {  copyPosition } from '../../helper'
-import {  useRef } from 'react'
-import { useAppContext } from '../../contexts/Context'
-import { makeNewMove, clearCandidates} from '../../reducer/actions/move'
-const Pieces =  () => {
-    
-    const {appState, dispatch} = useAppContext( )
-    // const [state,setState] = useState(createPosition())
-    const ref = useRef()
+import "./Pieces.css";
+import Piece from "./Piece";
+// import { copyPosition } from "../../helper";
+import { useRef } from "react";
+import { useAppContext } from "../../contexts/Context";
+import { makeNewMove, clearCandidates } from "../../reducer/actions/move";
+import arbiter from "../../arbiter/arbiter";
+const Pieces = () => {
+  const { appState, dispatch } = useAppContext();
+  // const [state,setState] = useState(createPosition())
+  const ref = useRef();
 
-    const currentPosition = appState.positions[appState.positions.length - 1]
+  const currentPosition = appState.positions[appState.positions.length - 1];
 
-    const calculateCoords = e =>{
-        const {width, left, top} = ref.current.getBoundingClientRect();
-        const size = width / 8
-        const y = Math.floor((e.clientX - left) / size)
-        const x = 7-Math.floor((e.clientY - top) / size)
-        return {x,y};
+  const calculateCoords = (e) => {
+    const { width, left, top } = ref.current.getBoundingClientRect();
+    const size = width / 8;
+    const y = Math.floor((e.clientX - left) / size);
+    const x = 7 - Math.floor((e.clientY - top) / size);
+    return { x, y };
+  };
+
+  const move = (e) => {
+    // console.log(e.clientX, e.clientY);
+    const { x, y } = calculateCoords(e);
+    const [piece, rank, file] = e.dataTransfer.getData("text").split(",");
+    if (appState.candidateMoves?.find((m) => m[0] === x && m[1] === y)) {
+      const newPosition = arbiter.performMove({
+        position: currentPosition,
+        piece,
+        rank,
+        file,
+        x,
+        y,
+      });
+      dispatch(makeNewMove({ newPosition }));
     }
+    dispatch(clearCandidates());
+  };
+  //     const newPosition = copyPosition(currentPosition)
+  //         if(p.endsWith('p') && newPosition[x][y] === '' && rank !== x && file !== y){
+  //             newPosition[rank][y] = ''
+  //         }
+  //         newPosition[rank][file] = ''
+  //         newPosition[x][y] = p
+  //         dispatch(makeNewMove({newPosition}))
+  //     dispatch(clearCandidates())
+  // }
 
-    const onDrop = e =>{
-        const newPosition = copyPosition(currentPosition)
+  const onDrop = (e) => {
+    e.preventDefault();
 
-        // console.log(e.clientX, e.clientY);
-        const {x,y} = calculateCoords(e)
-        const [p,rank,file] = e.dataTransfer.getData('text').split(',')
+    move(e);
+  };
+  const onDragOver = (e) => e.preventDefault();
 
-        if (appState.candidateMoves?.find(m => m[0] === x && m[1] === y)){
-            if(p.endsWith('p') && newPosition[x][y] === '' && rank !== x && file !== y){
-                newPosition[rank][y] = ''
-            }
-            newPosition[rank][file] = ''
-            newPosition[x][y] = p
-            dispatch(makeNewMove({newPosition}))
-        }
-
-        dispatch(clearCandidates())
-
-    }
-    const onDragOver = e => e.preventDefault()
-
-    return <div
-        className='pieces'
-        ref={ref}
-        onDrop={onDrop}
-        onDragOver={onDragOver}>
-        {currentPosition.map((r,rank) => 
-            r.map((f,file) => 
-            currentPosition[rank][file]
-                ? <Piece
-                    key={rank+'-'+file}
-                    rank={rank}
-                    file={file}
-                    piece={currentPosition[rank][file]}
-                /> : null 
-            )
-        )}
+  return (
+    <div className="pieces" ref={ref} onDrop={onDrop} onDragOver={onDragOver}>
+      {currentPosition.map((r, rank) =>
+        r.map((f, file) =>
+          currentPosition[rank][file] ? (
+            <Piece
+              key={rank + "-" + file}
+              rank={rank}
+              file={file}
+              piece={currentPosition[rank][file]}
+            />
+          ) : null
+        )
+      )}
     </div>
-}
+  );
+};
 
-export default Pieces
+export default Pieces;
