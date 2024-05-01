25e9cf4d1610d26eee968fa06cfa9d762dd59882
floatingmess06 <amandashore2468@gmail.com>
2024-05-01 11:27:12 +0530

    C14 // added check conditions

--- src/arbiter/arbiter.js
+++ src/arbiter/arbiter.js
@@ -7,8 +7,10 @@ import {
   getPawnMoves,
   getPawnCaptures,
   getCastlingMoves,
+  getKingPosition,
+  getPieces,
 } from "./getMoves";
-import {movePiece, movePawn} from './move'
+import { movePiece, movePawn } from "./move";
 
 const arbiter = {
   getRegularMoves: function ({ position, piece, rank, file }) {
@@ -24,8 +26,16 @@ const arbiter = {
     else if (piece.endsWith("p"))
       return getPawnMoves({ position, piece, rank, file });
   },
-  getValidMoves: function ({ position, prevPosition,castleDirection, piece, rank, file }) {
+  getValidMoves: function ({
+    position,
+    prevPosition,
+    castleDirection,
+    piece,
+    rank,
+    file,
+  }) {
     let moves = this.getRegularMoves({ position, piece, rank, file });
+    const notInCheckMoves = [];
     if (piece.endsWith("p")) {
       moves = [
         ...moves,
@@ -38,14 +48,54 @@ const arbiter = {
         ...getCastlingMoves({ position, castleDirection, piece, rank, file }),
       ];
     }
-    return moves;
+
+    moves.forEach(([x, y]) => {
+      const positionAfterMove = this.performMove({
+        position,
+        piece,
+        rank,
+        file,
+        x,
+        y,
+      });
+
+      if (
+        !this.isPlayerInCheck({ positionAfterMove, position, player: piece[0] })
+      )
+        notInCheckMoves.push([x, y]);
+    });
+    return notInCheckMoves;
   },
-  performMove: function ({position,piece,rank,file,x,y}) {
-        if(piece.endsWith('p') ){
-            return movePawn({position,piece,rank,file,x,y})
-        }else{
-            return movePiece({position,piece,rank,file,x,y})
-        }
+  performMove: function ({ position, piece, rank, file, x, y }) {
+    if (piece.endsWith("p")) {
+      return movePawn({ position, piece, rank, file, x, y });
+    } else {
+      return movePiece({ position, piece, rank, file, x, y });
+    }
+  },
+
+  isPlayerInCheck: function ({ positionAfterMove, position, player }) {
+    const enemy = player.startsWith("w") ? "b" : "w";
+    let kingPos = getKingPosition(positionAfterMove, player);
+    const enemyPieces = getPieces(positionAfterMove, enemy);
+
+    const enemyMoves = enemyPieces.reduce(
+      (acc, p) =>
+        acc = [
+          ...acc,
+          ...(p.piece.endsWith("p"))
+            ? getPawnCaptures({
+                position: positionAfterMove,
+                prevPosition: position,
+                ...p,
+              })
+            : this.getRegularMoves({ position: positionAfterMove, ...p }),
+        ],
+      []
+    );
+    if (enemyMoves.some(([x, y]) => kingPos[0] === x && kingPos[1] === y))
+      return true;
+    return false;
   },
 };
 
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,6 +4,8 @@ import "./Board.css";
 import Pieces from "../Pieces/Pieces";
 import { useAppContext } from "../../contexts/Context";
 import Popup from "../Popup/Popup";
+import arbiter from "../../arbiter/arbiter";
+import { getKingPosition } from "../../arbiter/getMoves";
 
 const Board = () => {
   const ranks = Array(8)
@@ -16,32 +18,49 @@ const Board = () => {
   const { appState } = useAppContext();
   const position = appState.positions[appState.positions.length - 1];
 
+  const isChecked = (() => {
+    const isInCheck = arbiter.isPlayerInCheck({
+      positionAfterMove: position,
+      player: appState.turn,
+    });
+    if (isInCheck) {
+      return getKingPosition(position, appState.turn);
+    }
+    return null;
+  })();
+
   const getClassName = (i, j) => {
-    let c = 'tile ';
-    c += (i + j) % 2 === 0 ? 'tile--dark ' : 'tile--light ';
-    if (appState.candidateMoves?.find(m => m[0] === i && m[1] === j)){
-      if (position[i][j]) c += 'attacking'
-      else c += 'highlight'
+    let c = "tile ";
+    c += (i + j) % 2 === 0 ? "tile--dark " : "tile--light ";
+    if (appState.candidateMoves?.find((m) => m[0] === i && m[1] === j)) {
+      if (position[i][j]) c += "attacking";
+      else c += "highlight";
+    }
+
+    if (isChecked && isChecked[0] === i && isChecked[1] === j) {
+      c += " checked";
     }
     return c;
   };
 
-  return <div className='board'>
+  return (
+    <div className="board">
       <Ranks ranks={ranks} />
-      <div className='tiles'>
+      <div className="tiles">
         {ranks.map((rank, i) =>
           files.map((file, j) => (
             <div
-              key={file+''+rank}
+              key={file + "" + rank}
               className={`${getClassName(7 - i, j)}`}
             ></div>
           ))
         )}
       </div>
-      <Pieces/>
+      <Pieces />
       <Popup />
       <Files files={files} />
     </div>
-}
+  );
+};
 
-export default Board
+export default Board;
